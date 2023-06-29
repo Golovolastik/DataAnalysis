1ecd7fdef060bafb5368e04300b19d78c2dcf5a5
Golovolastik <yankovich.aliaksei@gmail.com>
2023-06-29 20:20:37 +0300

    feature: add button

--- layout.py
+++ layout.py
@@ -3,6 +3,7 @@ import pandas as pd
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import ttk
+from selectionBlock import SelectionBlock
 
 class MyApp:
     def __init__(self, root):
@@ -16,6 +17,7 @@ class MyApp:
         # Создание первой вертикальной части (комбобокс и набор виджетов)
         self.combobox_frame = ttk.Frame(self.root, width=200, height=200)
         self.combobox_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
         
         # Создание второй вертикальной части (график)
         self.plot_frame = ttk.Frame(self.root, width=400, height=400)
@@ -28,9 +30,11 @@ class MyApp:
         # Создание комбобокса
         self.combobox = ttk.Combobox(self.combobox_frame)
         self.combobox.pack(padx=10, pady=10)
+        self.add_button = ttk.Button(self.combobox_frame, text="Добавить", command=self.add_selection_block)
+        self.add_button.pack(padx=10, pady=10)
         
         # Установка обработчика события выбора значения в комбобоксе
-        self.combobox.bind("<<ComboboxSelected>>", self.show_selected_value)
+        #self.combobox.bind("<<ComboboxSelected>>", self.show_selected_value)
         
         # Чтение данных из CSV-файла
         df = pd.read_csv("sample.csv", sep=';')
@@ -40,20 +44,6 @@ class MyApp:
         
         # Установка значений комбобокса как названия колонок
         self.combobox['values'] = column_names
-        
-        # Создание блока с виджетами
-        self.selected_value_block = ttk.Frame(self.combobox_frame)
-        self.selected_value_block.pack(padx=10, pady=10)
-        
-        # Создание виджетов в блоке
-        self.selected_value_label = ttk.Label(self.selected_value_block, text="Название параметра:")
-        self.selected_value_label.pack()
-        
-        self.selected_value_entry = ttk.Entry(self.selected_value_block)
-        self.selected_value_entry.pack(pady=10)
-        
-        self.save_button = ttk.Button(self.selected_value_block, text="Сохранить", command=self.save_value)
-        self.save_button.pack()
         
         # Создание графика
         fig = Figure(figsize=(5, 4), dpi=100)
@@ -78,16 +68,32 @@ class MyApp:
             table.insert("", "end", values=row)
         
         table.pack(padx=10, pady=10)
-    
+
+    def add_selection_block(self):
+        # Создание блока с виджетами
+        self.selected_value_block = ttk.Frame(self.combobox_frame)
+        self.selected_value_block.pack(padx=10, pady=10)
+
+        name = self.combobox.get()
+        # Создание виджетов в блоке
+        self.selected_value_label = ttk.Label(self.selected_value_block, text=f"{name}")
+        self.selected_value_label.pack()
+        
+        self.selected_value_entry = ttk.Entry(self.selected_value_block)
+        self.selected_value_entry.pack(pady=10)
+        
+        self.save_button = ttk.Button(self.selected_value_block, text="Удалить", command=lambda: self.delete_selection_block())
+        self.save_button.pack()
+
+    def delete_selection_block(self, block):
+        print("Hello")
+
     def show_selected_value(self, event):
         selected_value = self.combobox.get()
         self.selected_value_label['text'] = f"{selected_value}"
         self.selected_value_entry.delete(0, tk.END)
         self.save_button['state'] = tk.NORMAL
         
-    def save_value(self):
-        value = self.selected_value_entry.get()
-        # Действия по сохранению значения
         
 # Создание экземпляра приложения
 root = tk.Tk()
